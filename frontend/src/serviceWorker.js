247987a1315a9421285f24c0ddc6893b36ad3aea
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2024-09-12 12:36:02 +0200

    Troubleshooting offline opening of app

--- frontend/src/serviceWorker.js
+++ frontend/src/serviceWorker.js
@@ -1,48 +1,43 @@
 /* eslint-disable no-restricted-globals */
 
 const CACHE_NAME = "version-1";
-const urlsToCache = ['index.html', 'offline.html', '/static/js/main.7c957e3d.js', '/static/css/styles.scss'];
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/offline.html',        // Offline fallback page
+  '/static/js/main.js',   // Main JS bundle
+  '/static/css/main.css', // Main CSS bundle
+  // Include any other critical assets such as logos
+];
 
-// Install a service worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-          return cache.addAll([
-              'index.html',
-              'offline.html',
-              '/',
-              // Dynamically cache all static files
-              '/static/js/',
-              '/static/css/'
-          ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(() => {
+        return fetch(event.request).catch(() => caches.match('offline.html'));
       })
   );
 });
 
-// Listen for requests
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(() => {
-                return fetch(event.request)
-                    .catch(() => caches.match('offline.html'))
-            })
-    )
-});
-
-// Activate the service worker
 self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
-
-    event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if (!cacheWhitelist.includes(cacheName)) {
-                    return caches.delete(cacheName);
-                }
-            })
-        ))
-
-    )
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
