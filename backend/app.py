dc6195ad4262175654a33c85cf36dd4e71a3003c
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2024-10-04 18:12:02 +0200

    updated routing for React and Flask

--- backend/app.py
+++ backend/app.py
@@ -186,6 +186,15 @@ def load_detail_data():
     return hierarchical_data
 
 # Serve React App
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def serve_react_app(path):
+#     if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+#         return send_from_directory(app.static_folder, path)
+#     else:
+#         return send_from_directory(app.static_folder, 'index.html')
+
+# Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve_react_app(path):
@@ -193,6 +202,7 @@ def serve_react_app(path):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
+
 
 
 # API route to fetch audit header data
