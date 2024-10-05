9213c232fa08db6d2978b257a0929f00a547c7d0
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2024-10-05 10:26:39 +0200

    updated routing for React and Flask

--- backend/app.py
+++ backend/app.py
@@ -489,8 +489,8 @@ def serve_react_app(path):
     if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
     else:
+        print(f"Serving index.html for path: {path}")
         return send_from_directory(app.static_folder, 'index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
